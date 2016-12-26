2bf96fe4d94718d0ae8d7d3a3d12a67907da3664
delrandall <del_randall@hotmail.com>
2016-12-26 16:46:04 -0600

    Update BamazonCustomer.js

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -5,9 +5,7 @@ var Table = require('cli-table')
 var connection = mysql.createConnection({
 	host: 'localhost',
 	port: 3306,
-	//user: 'root',
 	user: '',
-	//password: secretKey.password,
 	password: '',
 	database: 'Bamazon'
 })
